b3a1bde1ab47bf6c4874fdc75dda68662154d2cb
Alex Brito <ajbrito93@gmail.com>
2024-09-13 13:43:30 -0700

    Added FAB, Wish, and WishItem composable

--- app/src/main/java/com/example/wishlistapp/composables/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/composables/AppBar.kt
@@ -1,27 +1,51 @@
 package com.example.wishlistapp.composables
 
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.wishlistapp.R
 
+private val WISHLIST: String = "WishList"
 
 @Composable
 fun AppBarView(
     title: String,
     onBackNavCicked: () -> Unit = {}
 ) {
+
+    val navigationIcon: (@Composable () -> Unit)? = {
+
+        if (!title.equals(WISHLIST)) {
+            IconButton(onClick = { onBackNavCicked() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+        } else {
+            null
+        }
+    }
+
+
     TopAppBar(
         title = { Text(text = title, color = colorResource(id = R.color.white)) },
-        modifier = Modifier
-            .padding(start = 4.dp)
-            .heightIn(max = 24.dp),
+        modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars),
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
+
     )
 }
--- app/src/main/java/com/example/wishlistapp/composables/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/composables/HomeView.kt
@@ -1,15 +1,51 @@
 package com.example.wishlistapp.composables
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.Wish
 
 @Composable
 fun HomeView() {
 
     Scaffold(topBar = {
-        AppBarView(title = "WishList")
-    }) { innerPadding ->
+        AppBarView(
+            title = "WishList",
+        )
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    /** TODO Navigation Functionality **/
+                },
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    tint = LocalContentColor.current
+                )
+            }
+        }) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
 
-        val innerPadding = innerPadding
+        }
     }
 }
